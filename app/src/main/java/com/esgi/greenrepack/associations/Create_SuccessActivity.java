55bf416ad683f36d57ad8f131082e2c172094ccc
LUHAKA <gluhaka@gmail.com>
2021-09-29 12:49:24 +0200

    Detail Green Coin, Liste Project, Add Project

---
+++ app/src/main/java/com/esgi/greenrepack/associations/Create_SuccessActivity.java
@@ -0,0 +1,16 @@
+package com.esgi.greenrepack.associations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esgi.greenrepack.R;
+
+public class Create_SuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_success);
+    }
+}
---
+++ app/src/main/java/com/esgi/greenrepack/associations/ProjectAddActivity.java
@@ -0,0 +1,16 @@
+package com.esgi.greenrepack.associations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.esgi.greenrepack.R;
+
+public class ProjectAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_add);
+    }
+}
