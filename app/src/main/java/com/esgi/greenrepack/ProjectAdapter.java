4ed082fad0d44b424fe51643175a32862839ab07
LUHAKA <gluhaka@gmail.com>
2021-09-29 21:16:05 +0200

    Detail Green Coin, Liste Project, Add Project

--- app/src/main/java/com/esgi/greenrepack/ProjectAdapter.java
+++ app/src/main/java/com/esgi/greenrepack/ProjectAdapter.java
@@ -11,32 +11,52 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.esgi.greenrepack.obj.Association;
+import com.esgi.greenrepack.obj.Project;
+
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.MyViewHolder> {
     @NonNull
     @NotNull
 
+    List<Project> projectListII = new ArrayList<>();
+    List<Project> ProjectListIII;
+    List<Project> ProjectListIV;
+
     int ProjectList[] = {R.drawable.travauxmaritime, R.drawable.ponttillionville,R.drawable.traintravaux};
-    String Labels[];
+
+    List<String> Labels;
+    String LabelsX[];
     Context context;
 
     public ProjectAdapter(int[] projectList, String[] labels, Context context) {
         ProjectList = projectList;
-        Labels = labels;
+        LabelsX = labels;
         this.context = context;
     }
+
+    /*public ProjectAdapter(List<Project> projectListII, List<String> labels, Context context) {
+        this.projectListII = projectListII;
+        Labels = labels;
+        this.context = context;
+    }*/
+
     public ProjectAdapter.MyViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
 
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.project_label_row,parent,false);
         return new ProjectAdapter.MyViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull ProjectAdapter.MyViewHolder holder, int position) {
 
-        holder.ProjectLabel.setText(Labels[position]);
+        holder.ProjectLabel.setText(projectListII.get(position).getLibelle());
         holder.Projectpicture.setImageResource(ProjectList[position]);
 
     }
@@ -50,6 +70,8 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.MyViewHo
 
         TextView ProjectLabel;
         ImageView Projectpicture;
+
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
--- app/src/main/java/com/esgi/greenrepack/ProjectsActivity.java
+++ app/src/main/java/com/esgi/greenrepack/ProjectsActivity.java
@@ -6,19 +6,44 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+
+import com.esgi.greenrepack.obj.Association;
+import com.esgi.greenrepack.obj.Project;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProjectsActivity extends AppCompatActivity {
 
+    List<Project> projectListII = new ArrayList<>();
+    List<Association> AssociationListe = new ArrayList<>();
+    private TextView Title;
 
-    int ProjectList[] = {R.drawable.travauxmaritime, R.drawable.ponttillionville,R.drawable.traintravaux, R.drawable.travauxmaritime, R.drawable.ponttillionville,R.drawable.traintravaux,};
-    String Labels[] = {"Port Lagos","Pont Algerie","Feroviere Turquie","Port Lagos","Pont Algerie","Feroviere Turquie"};
+    int ProjectList[] = {R.drawable.travauxmaritime, R.drawable.ponttillionville,R.drawable.traintravaux, R.drawable.travauxmaritime, R.drawable.ponttillionville,R.drawable.traintravaux};
+    List<String> Labels = new ArrayList<>();// {"Port Lagos","Pont Algerie","Feroviere Turquie","Port Lagos","Pont Algerie","Feroviere Turquie"};
+    String LabelsX[] = {"Port Lagos","Pont Algerie","Feroviere Turquie","Port Lagos","Pont Algerie","Feroviere Turquie"};
     RecyclerView recyclerViewX;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_list);
+
+
+        Title = this.findViewById(R.id.First_Association);
+
+        AssociationGenerate(AssociationListe);
+
+        for (int i = 0; i < AssociationListe.size() ; i++) {
+
+            String Label = AssociationListe.get(i).getNom();
+            Labels.add(Label);
+
+        };
+        //Title.setText(AssociationListe.get(1).getNom());
 
         ListeGenerate(R.id.Project_Liste);
         ListeGenerate(R.id.Project_ListeII);
@@ -35,12 +60,38 @@ public class ProjectsActivity extends AppCompatActivity {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);
         recyclerViewX.setLayoutManager(mLayoutManager);
 
-        RecyclerView.Adapter recyclerViewAdapter = new ProjectAdapter(ProjectList, Labels, this);
+        RecyclerView.Adapter recyclerViewAdapter = new ProjectAdapter(ProjectList, LabelsX, this);
         recyclerViewX.setAdapter(recyclerViewAdapter);
+
         return recyclerViewX;
 
-
-
     }
+
+
+    public List<Association> AssociationGenerate(List<Association> Associations){
+
+        Associations = AssociationListe;
+
+        Associations.add(0,new Association(1,"1000 Espoir","1000@milespoir.com","XDCDCFEVEAVA","","",projectListII));
+        Associations.add(1,new Association(2,"Adrien D","1000@milespoir.com","14545121VEFCQCQ","","",projectListII));
+        Associations.add(2,new Association(3,"Fabien R","1000@milespoir.com","ZDFaf64546216","","",projectListII));
+
+        Title.setText(AssociationListe.get(1).getNom());
+        return AssociationListe;
+    }
+
+    public List<Project> ProjectListeGenerate(List<Project> ProjectListII){
+
+
+        projectListII.add(1, new Project(3,"FirsP","Thats Third Project",240.21,"24/10/2021","24/08/2021",3));
+        projectListII.add(2, new Project(2,"FirsPV","Thats Seconde Project",240.21,"24/10/2021","24/08/2021",2));
+        projectListII.add(3, new Project(3,"FirsXP","Thats Seconde Project",240.21,"24/10/2021","24/08/2021",3));
+        projectListII.add(1, new Project(4,"Firs11","Thats My First P11",240.21,"24/10/2021","24/08/2021",1));
+        projectListII.add(2, new Project(5,"Firs22","Thats My First P22",240.21,"24/10/2021","24/08/2021",2));
+        projectListII.add(3, new Project(6,"First33","Thats My First P33",240.21,"24/10/2021","24/08/2021",3));
+
+        return projectListII;
+    }
+
 
 }
